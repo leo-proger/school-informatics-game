b77a87341687967325d4265b79ab36cbadda1254
Fllorovi <lukianovalexandr2@mail.ru>
2026-06-14 15:44:35 +0300

    моя игра

---
+++ components/ui/GlassPanel.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ReactNode } from "react";
+
+interface GlassPanelProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export default function GlassPanel({ children, className = "" }: GlassPanelProps) {
+  return (
+    <div
+      className={`
+        rounded-[28px] border border-cyan-400/30 bg-black/40 backdrop-blur-xl
+        shadow-[0_0_50px_rgba(34,211,238,.15)] overflow-hidden relative
+        before:absolute before:inset-0 before:bg-gradient-to-br before:from-cyan-500/5 before:to-purple-500/5 before:pointer-events-none
+        ${className}
+      `}
+    >
+      {children}
+    </div>
+  );
+}
